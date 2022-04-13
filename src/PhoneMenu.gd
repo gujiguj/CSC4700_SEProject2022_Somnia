131992e5d0a446149eaba4cd5ba18adadbfa07d7
Lucy Wang <43558154+gujiguj@users.noreply.github.com>
2022-04-12 21:29:42 -0400

    implemented a tasks popup

--- src/PhoneMenu.gd
+++ src/PhoneMenu.gd
@@ -5,15 +5,49 @@ extends CanvasLayer
 # var a = 2
 # var b = "text"
 onready var phone = $PhoneContainer/PhoneHomeContainer/PhoneHomeMenu
-
+onready var task_list = $Tasks/MarginContainer/TasksList
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	$Tasks.hide()
+	addTask(2, "Go to class")
+	addTask(1, "Do homework")
+	addTask(6, "Email professor")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
+#func _process(_delta):
 #	pass
 
+func _on_PhoneHomeMenu_item_selected(index):
+	match(index):
+		1:
+			$Tasks.show()
+
+func _on_CloseTasksButton_pressed():
+	$Tasks.hide()
+	
+func addTask(time_left, task):
+	task_list.add_item(str(time_left))
+	task_list.add_item(task)
+	
+func subtractTime(time):
+	var remove_queue = []
+	for index in task_list.get_item_count():
+		if index % 2 == 0:
+			print("Subtracting ", time, " hours from ", index)
+			var text = float(task_list.get_item_text(index))
+			print("Current time left: ", str(text))
+			text -= time
+			task_list.set_item_text(index, str(text)) 
+			print("New time left: ", task_list.get_item_text(index))
+			if text <= 0:
+				remove_queue.push_front(index)
+				remove_queue.push_front(index+1)
+	for index in remove_queue:
+		task_list.remove_item(index)
+		
+func formatTime(time):
+	# converts a float value of hours to a format of H:M
+	# may implement this later because this is dumb
+	pass
