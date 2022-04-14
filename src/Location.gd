69acd2f4643dad4a8982cde99eb6f05b6eaf5211
Lucy Wang <43558154+gujiguj@users.noreply.github.com>
2022-04-14 16:06:09 -0400

    creation of new signal to open map

--- src/Location.gd
+++ src/Location.gd
@@ -1,19 +1,21 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Declare member variables here. 
 onready var choices = $LocationBg/ChoicesContainer
 onready var choice1 = $LocationBg/ChoicesContainer/VBoxContainer/Choice1
 onready var choice2 = $LocationBg/ChoicesContainer/VBoxContainer/Choice2
 onready var choice3 = $LocationBg/ChoicesContainer/VBoxContainer/Choice3
 onready var choice_leave = $LocationBg/ChoicesContainer/VBoxContainer/Leave
 
+# to be used to open up the map
+signal leave_location
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	choice1.text = ""
+	choice2.text = ""
+	choice3.text = ""
+	choice_leave = "Leave"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -21,15 +23,21 @@ func _ready():
 
 func _on_Choice1_pressed():
 	choice1.disabled = true
+	# include task functions if applicable
 
 func _on_Choice2_pressed():
 	choice2.disabled = true
+	# include task functions if applicable
 
 func _on_Choice3_pressed():
 	choice3.disabled = true
+	# include task functions if applicable
 	
+# opens a confirmation dialog asking if player wants to leave
 func _on_Leave_pressed():
 	$LocationBg/ConfirmationDialog.popup()
 
+# emits a signal to open up the map when the player confirms they want to leave
 func _on_ConfirmationDialog_confirmed():
 	choices.hide()
+	emit_signal("leave_location")
