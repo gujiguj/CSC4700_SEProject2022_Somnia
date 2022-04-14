c3783a8f848d3dda854abc6d928739ef4e06457d
Lucy Wang <43558154+gujiguj@users.noreply.github.com>
2022-04-14 16:39:06 -0400

    create a very simple clock (or timer, really)

---
+++ src/Clock.gd
@@ -0,0 +1,34 @@
+extends MarginContainer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+onready var clock = $ClockText
+
+# signal emitted when the clock is 0
+signal day_over
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	clock.text = "8"
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+# func _process(delta):
+#	if Input.is_action_just_pressed("ui_accept"):
+#		passTime(0.5)
+#	pass
+
+# input: float representing hours passed
+# used to decrease time on the clock 
+# emits day over signal if time is less than or equal to 0
+func passTime(hours):
+	var curr_time = float(clock.text)
+	var new_time = curr_time - hours
+	clock.text = str(new_time)
+	if new_time <= 0:
+		emit_signal("day_over")
+	
+func formatTime():
+	pass
