739ecb731eedefac4ef38f367cd46e2680b09486
Lucy Wang <43558154+gujiguj@users.noreply.github.com>
2022-04-14 15:32:42 -0400

    implemented disable/enable functions

--- src/Map.gd
+++ src/Map.gd
@@ -5,15 +5,32 @@ extends Control
 # var a = 2
 # var b = "text"
 
+onready var dorm = $MapTexture/DormButton
+onready var library = $MapTexture/LibraryButton
+onready var dining_hall = $MapTexture/DiningHallButton
+onready var building = $MapTexture/BuildingButton
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-
+	disableButtons()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+# disables all location buttons
+func disableButtons():
+	dorm.disabled = true
+	library.disabled = true
+	dining_hall.disabled = true
+	building.disabled = true
+	
+# enables all location buttons
+func enableButtons():
+	dorm.disabled = false
+	library.disabled = false
+	dining_hall.disabled = false
+	building.disabled = false
 
 func _on_DormButton_pressed():
 	$ConfirmationDialog.window_title = "Go to dorm"
@@ -34,7 +51,6 @@ func _on_BuildingButton_pressed():
 	$ConfirmationDialog.window_title = "Go to class"
 	$ConfirmationDialog.dialog_text = "Do you want to go to class?"
 	$ConfirmationDialog.popup_centered()
-
 
 func _on_ConfirmationDialog_confirmed():
 	$MapTexture.hide()
