bb0bdd605fc43f5cacd721223ad972a64f8ad660
lrosari1 <lrosari1@villanova.edu>
2022-04-20 00:39:06 -0400

    Added NightTutorial

---
+++ src/DownArrow.gd
@@ -0,0 +1,11 @@
+extends RigidBody2D
+
+var movement = Vector2()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func _physics_process(delta):
+	movement.y = 150*delta
+	translate(movement)
